cb878367e12702a85cf86eb0bd6eeb658b3fe3a6
Sanjit Dutta <sdlynx@gmail.com>
2016-02-29 21:15:00 -0600

    working on MP2, up to carousel

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,17 @@
-// Write any custom javascript functions here
+$(document).foundation();
+
+$(function() {
+
+	$('.sticky-full').stickyNavbar({
+		mobile: true,
+		startAt: 0,
+		jqueryAnim: "show",
+		easing: "easeInOutQuart"
+	});
+
+	$('#mainNav ul li a').on("click", function(e) {
+		if(!Foundation.MediaQuery.atLeast('large')) $('#mainNav').css('display','none');
+	});
+
+	$('#carousel').slick();
+});
