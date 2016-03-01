a8a9ed7348a67fe45707b97b8095052d12f811e4
Sanjit Dutta <sdlynx@gmail.com>
2016-03-01 01:05:27 -0600

    everything working, modal kinda crappy

--- source_js/script.js
+++ source_js/script.js
@@ -2,16 +2,43 @@ $(document).foundation();
 
 $(function() {
 
+	// sticky navbar
+
 	$('.sticky-full').stickyNavbar({
 		mobile: true,
-		startAt: 0,
 		jqueryAnim: "show",
 		easing: "easeInOutQuart"
 	});
 
 	$('#mainNav ul li a').on("click", function(e) {
-		if(!Foundation.MediaQuery.atLeast('large')) $('#mainNav').css('display','none');
+		if(!Foundation.MediaQuery.atLeast('large')) $('#mainNav').slideUp();
 	});
 
-	$('#carousel').slick();
+	// slick carousel
+
+	$('#carousel').slick({
+		autoplay: true,
+		appendArrows: "#carousel",
+		prevArrow: "#prev",
+		nextArrow: "#next",
+		cssEase: 'cubic-bezier(0.455, 0.030, 0.515, 0.955)',
+		useTransform: true
+	});
+
+	// de-obfuscate email address on page
+
+	var link = "<n uers=\"znvygb:ebjsbhaqngvba@irevmba.arg\" ery=\"absbyybj\">ebjsbhaqngvba@irevmba.arg</n>".replace(/[a-zA-Z]/g, 
+		function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+	$('#ROWemail').html(link);
+
+	// shrink navbar when scroll down
+	// 
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > 200) {
+			$('.sticky-full').addClass('shrink');
+		} else {
+			$('.sticky-full').removeClass('shrink');
+		}
+	});
+
 });
